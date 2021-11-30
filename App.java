a78033c6f984620532ae0a60154b4e18710c6e20
s1960565 <80416189+s1960565@users.noreply.github.com>
2021-11-30 18:08:32 +0000

    Add files via upload

--- App.java
+++ App.java
@@ -10,10 +10,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -21,8 +17,8 @@ public class App
 
         final double APPLETON_LONGITUDE = -3.186874;
         final double APPLETON_LATITUDE = 55.944494;
+        Point appleton = Point.fromLngLat(APPLETON_LONGITUDE, APPLETON_LATITUDE);
 
-        //final HttpClient client = HttpClient.newHttpClient(); //Only 1 client is created for all requests.
         String date = args[0];
         String month = args[1];
         String year = args[2];
@@ -31,90 +27,40 @@ public class App
         Date fullDate = Date.valueOf(args[2] + "-" + args[1] + "-" + args[0]);
 
 
+        Drone drone = new Drone();
         Orders orders = new Orders(dbPort, fullDate);
-
         Menus menu = new Menus(webPort);
-        Map<String, LongLat> shopsToLongLat = menu.getShopsToLongLat();
-        //YOU HAVE SHOPS TO LONGLAT YOU HAVE A ARRAY OF SHOPS FOR A ORDER YOU HAVE MAP (IN ORDERS CLASS) THAT MAPS ORDER NO TO LONGLAT OF DELIVERYLOCATION
-        //CREATE MAP THAT MAPS ORDERNO TO LIST OF SHOPS
-        //
-
-        Map<String, Integer> ordersSortedByValue = Orders.getOrderedValuableOrdersToCostMap(menu);
-
-
         Buildings buildings = new Buildings(webPort);
+
+        Map<String, LongLat> shopsToLongLat = menu.getShopsToLongLat();
+        Map<String, Integer> ordersSortedByValue = Orders.getOrderedValuableOrdersToCostMap(menu);
+        Map<String, LongLat> OrderNoToDeliverToLongLat = Orders.getOrderNoToDeliverToLongLat(webPort);
+
         List<Point> landmarkPoints = Buildings.landmarkPoints;
-        Point appleton = Point.fromLngLat(APPLETON_LONGITUDE, APPLETON_LATITUDE);
         landmarkPoints.add(appleton);
 
-
-
-        //LongLat drone = new LongLat(topMeadows.lng, topMeadows.lat);
-
-        Drone drone = new Drone();
-        //drone.position = new LongLat(LongLat.APPLETON_LONGITUDE, LongLat.APPLETON_LATITUDE);
-
-
-
-        //int bestAngle;
-
+        //We add Appleton Tower to our path since we always start from there.
         List<Point> path = new ArrayList<>();
         path.add(Point.fromLngLat(appleton.longitude(), appleton.latitude()));
 
-        //List<Point> landmarkPoints = new ArrayList<>();
-
-
-
-
-
-        /**
-         while (!drone.position.closeTo(dest)){
-
-         LongLat currentPosition = drone.position;
-
-         if (!buildings.checkDirectRoute(currentPosition, dest)){
-         LongLat closestLandmark = new LongLat(999,999);
-         double closestLandmarkDistance = 999;
-         for (Point landmarkPoint : landmarkPoints){
-         LongLat landmarkLongLat = new LongLat(landmarkPoint.longitude(), landmarkPoint.latitude());
-         if (buildings.checkDirectRoute(currentPosition, landmarkLongLat)
-         && dest.distanceTo(landmarkLongLat) < closestLandmarkDistance){
-         closestLandmark = landmarkLongLat;
-         closestLandmarkDistance = dest.distanceTo(landmarkLongLat);
-         }
-         }
-         while (!drone.position.closeTo(closestLandmark)){
-         currentPosition = drone.position;
-         bestAngle = currentPosition.bestAngle(closestLandmark);
-         pl.add(Point.fromLngLat(drone.position.lng, drone.position.lat));
-         drone.fly(bestAngle);
-         drone.battery -= 1;
-         }
-         }
-         else{
-         bestAngle = currentPosition.bestAngle(kfc);
-         pl.add(Point.fromLngLat(drone.position.lng, drone.position.lat));
-         drone.fly(bestAngle);
-         drone.battery -= 1;
-         }
-         }
-
-         **/
-        //path = drone.algorithm(landmarkPoints, dest, buildings, path);
-        //Map<String, LongLat> shopsToLongLat = menu.getShopsToLongLat();
-
-        String currentOrderNo = null;
-
+        //Calculating the total monetary value of orders placed on the given date.
         double totalMonetaryValuePlaced = 0;
         double monetaryValue = 0;
         for (double orderCost : ordersSortedByValue.values()){
             totalMonetaryValuePlaced += orderCost;
         }
 
-        Map<String, LongLat> OrderNoToDeliverToLongLat = Orders.getOrderNoToDeliverToLongLat(webPort);
+        String currentOrderNo = null;
+
+        int counter2 = 0;
 
         topLoop:
         for (String orderNo : ordersSortedByValue.keySet()){
+            counter2+=1;
+            System.out.println(counter2);
+            if (counter2 == 20){
+                counter2 = counter2;
+            }
             Collection<String> itemNames = Orders.getItemNamesFromOrder(orderNo);
             String[] shopsToVisit = menu.shopsArrayFromItems(itemNames);
             LongLat deliverToLongLat = OrderNoToDeliverToLongLat.get(orderNo);
@@ -128,9 +74,11 @@ public class App
             }
             path = drone.algorithm(landmarkPoints, deliverToLongLat, buildings, path);
             int costInPenceOfOrder = ordersSortedByValue.get(orderNo);
+            //We keep updating this variable to find the total monetary value of orders delivered.
             monetaryValue += costInPenceOfOrder;
             Orders.insertIntoDeliveries(orderNo, costInPenceOfOrder);
 
+            //Beginning to write to the flightpath table.
             int counter = 0;
             for (LongLat moveFrom : drone.getMovesFrom()){
                 Orders.insertIntoFlightpath(orderNo, moveFrom.lng, moveFrom.lat,
@@ -138,16 +86,19 @@ public class App
                 counter+=1;
             }
 
+            //Since we are finished with the current order, and we have written everything relevant to the flightpath table
+            //about the order, we can clear the lists to begin with our next order.
             drone.setMovesFrom(new ArrayList<LongLat>());
             drone.setMovesTo(new ArrayList<LongLat>());
             drone.setAnglesOfMoves(new ArrayList<Integer>());
 
             currentOrderNo = orderNo;
-
         }
 
 
-        //this is for when you realise you have no moves left, you break out of the toploop but never insert into table
+        //this is for when we realise we have no moves left, we break out of the topLoop but never insert into table flightpath.
+        //This is why we had to keep a variable currentOrderNo to remember the orderNo in such a situation.
+        //Beginning to write to the flightpath table.
         int counter = 0;
         for (LongLat moveFrom : drone.getMovesFrom()){
             Orders.insertIntoFlightpath(currentOrderNo, moveFrom.lng, moveFrom.lat,
@@ -155,13 +106,17 @@ public class App
             counter+=1;
         }
 
+        //Since we are done with all the orders, and have written all the relevant information to the flightpath table about
+        //the orders, we can clear these lists so that they can be updated with the information on the flightpath back to
+        //appleton tower.
         drone.setMovesFrom(new ArrayList<LongLat>());
         drone.setMovesTo(new ArrayList<LongLat>());
         drone.setAnglesOfMoves(new ArrayList<Integer>());
 
-
+        //Path finding back to Appleton Tower.
         path = drone.algorithmEnd(landmarkPoints, buildings, path);
 
+        //Beginning to write to the flightpath table.
         counter = 0;
         for (LongLat moveFrom : drone.getMovesFrom()){
             Orders.insertIntoFlightpath(null, moveFrom.lng, moveFrom.lat,
@@ -169,37 +124,40 @@ public class App
             counter+=1;
         }
 
+        //Calculating the percentage monetary value delivered on the date in question.
         double percentageMonetaryValue = (monetaryValue / totalMonetaryValuePlaced) * 100;
-        System.out.println(percentageMonetaryValue);
-        System.out.println(drone.getMoves());
+        System.out.println("Percentage monetary value is " + percentageMonetaryValue + "%");
+        System.out.println(drone.getMoves() + " Moves made");
 
 
-        //CREATING THE GEOJSON FILE
+
         LineString x = LineString.fromLngLats(path);
         Geometry g = (Geometry)x;
         Feature f = Feature.fromGeometry(g);
         FeatureCollection fc = FeatureCollection.fromFeature(f);
 
+        //Creating the geoJson file that will hold information about the drones flightpath throughout the day.
         try {
             File myObj = new File("drone-" + date + "-" + month + "-" + year +".geojson");
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
 
             } else {
-                System.out.println("File already exists.");
+                System.out.println("GeoJson file " + myObj.getName() + " already exists.");
             }
         } catch (java.io.IOException e) {
-            System.out.println("An error occurred.");
+            System.err.println("An error occurred.");
             e.printStackTrace();
         }
 
+        //Writing to the geoJson file
         try {
             FileWriter myWriter = new FileWriter("drone-" + date + "-" + month + "-" + year +".geojson");
             myWriter.write(fc.toJson());
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (java.io.IOException e) {
-            System.out.println("An error occurred.");
+            System.err.println("An error occurred.");
             e.printStackTrace();
         }
     }
--- Drone.java
+++ Drone.java
@@ -20,6 +20,21 @@ public class Drone {
     private List<Integer> anglesOfMoves = new ArrayList<>();
 
 
+    /**
+     *
+     * Method that makes the drone fly toward a given angle.
+     * It will move 0.00015 towards the angle.
+     * The position of the drone is updated using the nexPosition method from LongLat class.
+     * Battery goes down 1 after one move.
+     * Our moves counter will go up 1.
+     * We also add the angle of travel in a list of angles that is added to the flightpath tables.
+     * We also add the position of the drone to a list before we flew to the next position, which is also for the flightpath table.
+     * We also add the position of the drone to a list after we fly to the next position, which is also for the flightpath table.
+     * These lists gets reset after each order is complete and added to the tables in the db.
+     *
+     * @param angle Integer angle in degrees is the direction which we would like to fly toward where we follow the convention that
+     *              0 degrees is east and 90 is north, etc...
+     */
     public void fly(int angle){
         movesFrom.add(position);
         position = position.nextPosition(angle);
@@ -29,6 +44,16 @@ public class Drone {
         anglesOfMoves.add(angle);
     }
 
+    /**
+     *
+     * Method that makes the drone hover in a position.
+     * The position after a hover move will be unchanged.
+     * The battery will go down by 1.
+     * Our moves counter will go up by 1.
+     * We add the same position to both movesFrom list and movesTo list.
+     * Since we are hovering, the angle is the junk value of -999, which is added to the list angleOfMoves.
+     *
+     */
     public void hover()
     {
         movesFrom.add(position);
@@ -38,6 +63,30 @@ public class Drone {
         anglesOfMoves.add(LongLat.JUNK_VALUE);
     }
 
+    /**
+     *
+     * This method is the main algorithm which controls the flightpath of the drone.
+     * This method is called if we want our drone to travel from point A to point B (destination).
+     * We first check if there is a direct route to the destination without any NFZ's blocking the path.
+     * If there is, then we simply take this route, while checking for unexpected NFZ's because of rounding the angle.
+     * If there is not, then we direct the drone towards the landmark which is closest to the destination, then after
+     * that it is sent toward the destination as before, if there is a direct route (which there should be).
+     * We repeat this until either we are close to the destination or our drone has enough battery to only return back
+     * to Appleton Tower, in which we break out of the method completely.
+     * After every move (fly or hover), we add the new position to a list of points, which will be used for the GeoJson file
+     * that we create.
+     *
+     *
+     * @param landmarkPoints List of points which contains the locations of all the landmarks, which we may divert toward
+     *                       if there is no direct route to the destination.
+     * @param destination LongLat object which is the point we are trying to move towards. This can be a landmark, shop or pick-up location.
+     * @param buildings Buildings object which contains the required information about the NFZ's that we check for when we
+     *                  move toward the destination.
+     * @param path List of points which we keep passing into this method until the end of the day.
+     *
+     * @return List of points which contains the drones positions move by move.
+     * By the end of the day, this list should contain every position the drone moved to in order.
+     */
     public List<Point> algorithm (List<Point> landmarkPoints, LongLat destination, Buildings buildings, List<Point> path){
 
         int bestAngle;
@@ -79,6 +128,19 @@ public class Drone {
         return path;
     }
 
+
+    /**
+     *
+     * This method works the same as the algorithm method above, however the destination is hardcoded as Appleton Tower.
+     * This method is used at the end of the day when we have either run out of moves or we have completed delivering the orders.
+     *
+     * @param landmarkPoints List of points which contains the locations of all the landmarks, which we may divert toward
+     *                        if there is no direct route to Appleton Tower.
+     * @param buildings Buildings object which contains the required information about the NFZ's that we check for when we
+     *                  move toward Appleton Tower.
+     * @param path List of points which, after calling this method for our main drone, should be the final time it is updated.
+     * @return List of points which contains the drones positions move by move throughout the day.
+     */
     public List<Point> algorithmEnd (List<Point> landmarkPoints, Buildings buildings, List<Point> path){
 
         int bestAngle;
@@ -103,22 +165,39 @@ public class Drone {
         return path;
     }
 
+    /**
+     *
+     * Method which runs the algorithmEnd on a dummy drone (which is of the same position of the main drone)
+     * and makes sure that the number of moves made by that dummy drone to get to Appleton Tower is at least 5 lower
+     * than the battery left in the main drone.
+     * We use the value 5 here as a safety net in case we need to make some unexpected adjustments to angles, causing
+     * us to make more moves than calculated.
+     *
+     *
+     * @param landmarkPoints List of points which contains the locations of all the landmarks, which is needed as a parameter
+     *                       for the algorithmEnd method.
+     * @param buildings Buildings object which contains the required information about the NFZ's that we need to pass in
+     *                  to the algorithmEnd method.
+     * @return Boolean value true if we need to stop with our deliveries because we don't have sufficient battery left,
+     *         or false if we can carry on with enough battery.
+     */
     public boolean outOfMoves(List<Point> landmarkPoints, Buildings buildings){
 
-        Drone dummyDrone = new Drone();
-        dummyDrone.position = position;
-        dummyDrone.moves = 0;
+        int SAFETY_BUFFER = 5;
+
+        LongLat.dummyDrone.position = position;
+        LongLat.dummyDrone.moves = 0;
+        LongLat.dummyDrone.battery = 1500;
 
         List<Point> dummyList = new ArrayList<>();
 
-        dummyDrone.algorithmEnd(landmarkPoints, buildings, dummyList);
+        LongLat.dummyDrone.algorithmEnd(landmarkPoints, buildings, dummyList);
 
-        return (battery - dummyDrone.moves < 5);
+        return (battery - LongLat.dummyDrone.moves < SAFETY_BUFFER);
 
     }
 
-    //Getters and Setters:
-
+    //Getters
     public LongLat getPosition(){
         return position;
     }
@@ -143,6 +222,7 @@ public class Drone {
         return anglesOfMoves;
     }
 
+    //Setters
     public void setPosition(LongLat newPosition){
         this.position = newPosition;
     }
